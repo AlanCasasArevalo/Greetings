4a3842931bc164d8303e1fe1a59cb2647b6bd29b
Alan Casas <alan.casas@edreamsodigeo.com>
2024-02-06 13:39:21 +0100

    Initial Commit

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by alan.casas on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
