814c65fb67d197969ddd482e63a859ff6fce478a
Alan Casas <alan.casas@edreamsodigeo.com>
2024-02-07 15:00:46 +0100

    Tips were added to our app

--- Greetings/GreetingsApp.swift
+++ Greetings/GreetingsApp.swift
@@ -1,15 +1,18 @@
 import SwiftUI
+import TipKit
 
 @main
 struct GreetingsApp: App {
     @AppStorage("language") var language: String = "en"
     @AppStorage("layoutDirectionString") var layoutDirectionString: String = LEFT_TO_RIGHT
-
+    
     var layoutDirection: LayoutDirection {
-        layoutDirectionString == LEFT_TO_RIGHT 
+        layoutDirectionString == LEFT_TO_RIGHT
         ? .leftToRight
         : .rightToLeft
     }
+    
+    let resetTip = false
     
     var body: some Scene {
         WindowGroup {
@@ -19,6 +22,18 @@ struct GreetingsApp: App {
             )
             .environment(\.locale, Locale(identifier: language))
             .environment(\.layoutDirection, layoutDirection)
+            .task {
+                if resetTip {
+                    // Resetea el counter de los tips y los vuelve a mostrar
+                    try? Tips.resetDatastore()
+                }
+                
+                // Este Tip son consejos de que hacer (estilo guide)
+                try? Tips.configure([
+                    .displayFrequency(.immediate),
+                    .datastoreLocation(.applicationDefault)
+                ])
+            }
         }
     }
 }
--- Greetings/Views/TitleView.swift
+++ Greetings/Views/TitleView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import TipKit
 
 /// This is the title view of the app wich
 /// displays a title, a subtitle, wich randomly changes on tap
@@ -6,10 +7,12 @@ import SwiftUI
 /// that is amazing
 struct TitleView: View {
     @State var subtitle: LocalizedStringKey = LocalizedStringKey("Exploring iOS Programming")
+    private var greetingsTip = GreetingTip()
   
     var body: some View {
         HStack{
             GreetingsTextView(subtitle: $subtitle)
+                .popoverTip(greetingsTip)
             Spacer()
             RotatableCircleView()
         }
